33d3d6bebcbaa811e823bdba27d34eb33068839b
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2021-12-02 15:44:00 +0100

    initial commit

---
+++ lab4_2021_MatWarasm.asm
@@ -0,0 +1,154 @@
+;***********************************************************
+; File Header
+;***********************************************************
+    list p=18F25k50, r=hex, n=0
+    #include <p18f25k50.inc>	; file to provide register definitions for the specific processor (a lot of equ)
+
+; File registers
+
+;***********************************************************
+; Reset Vector
+;***********************************************************
+
+    ORG     0x1000	; Reset Vector
+			; When debugging:0x0000; when loading: 0x1000
+    GOTO    START
+
+;***********************************************************
+; Interrupt Vector
+;***********************************************************
+
+    ORG     0x1008	; Interrupt Vector HIGH priority
+    GOTO    inter_high	; When debugging:0x008; when loading: 0x1008
+    ORG     0x1018	; Interrupt Vector LOW priority
+    GOTO    inter_low	; When debugging:0x0008; when loading: 0x1018
+
+;***********************************************************
+; Program Code Starts Here
+;***********************************************************
+
+    ORG     0x1020	; When debugging:0x020; when loading: 0x1020
+
+START
+    movlw   0x80	; load value 0x80 in work register
+    movwf   OSCTUNE		
+    movlw   0x70	; load value 0x70 in work register
+    movwf   OSCCON		
+    movlw   0x10	; load value 0x10 to work register
+    movwf   OSCCON2		
+    clrf    LATA 	; Initialize PORTA by clearing output data latches
+    movlw   0xFF 	; Value used to initialize data direction
+    movwf   TRISA 	; Set PORTA as output
+    movlw   0x00 	; Configure A/D for digital inputs 0000 1111
+    movwf   ANSELA	
+    movlw   0x00	; Configure comparators for digital input
+    movwf   CM1CON0
+    clrf    LATB	; Initialize PORTB by clearing output data latches
+    movlw   0x00	; Value used to initialize data direction
+    movwf   TRISB	; Set PORTB as output
+    clrf    LATC	; Initialize PORTC by clearing output data latches
+    movlw   0x01	; Value used to initialize data direction
+    movwf   TRISC	; Set RC0 as input
+    
+    ;enable timer0 interrupts
+    movlw   b'00100000'	;enables timer0 interrupts
+    movwf   INTCON
+    movlw   b'10000110';timer 0 geeft een interrupt met een frequentie van 0.7Hz
+    movwf   T0CON
+    bsf	    INTCON2,2;set bit 2, this set TMR0 interrupt to high priority
+
+    bcf     UCON,3	; to be sure to disable USB module
+    bsf     UCFG,3	; disable internal USB transceiver
+    
+    goto main		;goto main and start the code
+
+main
+    goto    loop
+    ;call    init_lut
+
+init_dac    
+    ;complete me
+    
+    bsf	    INTCON,GIE 
+    return
+loop
+    goto    loop
+
+;***********************************************************
+; subroutines
+;***********************************************************
+init_lut
+    ; FSR1: will go though table of sine samples; starts at address 0x010
+    lfsr    1, 0x010
+    movlw   D'15'	
+    movwf   0x10
+    movlw   D'21'	
+    movwf   0x11
+    movlw   D'26'	
+    movwf   0x12
+    movlw   D'29'	
+    movwf   0x13
+    movlw   D'30'	
+    movwf   0x14
+    movlw   D'29'	
+    movwf   0x15
+    movlw   D'26'	
+    movwf   0x16
+    movlw   D'21'	
+    movwf   0x17
+    movlw   D'15'	
+    movwf   0x18
+    movlw   D'9'	
+    movwf   0x19
+    movlw   D'4'	
+    movwf   0x1A
+    movlw   D'1'	
+    movwf   0x1B
+    movlw   D'0'	
+    movwf   0x1C
+    movlw   D'1'	
+    movwf   0x1D
+    movlw   D'4'	
+    movwf   0x1E
+    movlw   D'9'	
+    movwf   0x1F
+    
+    ; FSR0: song
+    ;complete me
+    
+    return
+    
+;***********************************************************
+; interrupt handling
+;***********************************************************
+
+inter_high
+    movff   0xFF,LATC
+    btfsc   INTCON,2	;will be high if timer0 has given interrupt
+    call ih_tmr0
+    retfie
+    
+
+ih_tmr1
+    
+    return
+
+ih_tmr0
+    btg	    LATC,1; toggle RC1 led
+    bcf	    INTCON,2;clear the flag bit
+    return
+    
+inter_low
+    nop
+    retfie
+;***********************************************************
+; table
+;***********************************************************
+    
+NOTES ;TMR1H TM1L
+    DB 0x00, 0x00   ;Silence?
+    ;calculate the correct values
+    DB 0xFF, 0xEE   ;C = do
+    ;...
+    
+    END
